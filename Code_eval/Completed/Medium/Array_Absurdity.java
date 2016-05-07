f45268f85dc4e57cd0ed531bfecc17d6c03631d4
Francesco Marazzi <francesco.marazzi90@gmail.com>
2016-05-07 17:42:56 +0200

    Completed ArrayAbs

---
+++ Code_eval/Completed/Medium/Array_Absurdity.java
@@ -0,0 +1,26 @@
+package Medium;
+import java.io.*;
+
+public class Array_Absurdity {
+	public static void main (String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+            String[] Split = line.split(";");
+            String[] Numbers = Split[1].split(",");
+            int length = Integer.parseInt(Split[0]);
+            Integer[] Test = new Integer[length];
+            
+            for(int i=0;i<length;i++){
+            	int index = Integer.parseInt(Numbers[i]);
+            	if(Test[index] == null) Test[index] = 1;
+            	else{ 
+            		System.out.println(index);
+            		break;
+            	}
+            }
+        } 
+    }
+}
